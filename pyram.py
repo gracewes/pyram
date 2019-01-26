fc074b806d93a30eb7fa07eec547bc989884514a
rawells14 <rawells14@gmail.com>
2019-01-26 14:17:51 -0600

    Scaffolding Completed

--- pyram.py
+++ pyram.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def home_page():
-    return 'Welcome to Pyram!'
+    return render_template('index.html')
